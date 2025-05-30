59b0a4456769a91d874f4f844a6a91e425a83fd8
Rutuga <m854480@gmail.com>
2025-05-30 20:08:17 +0700

    update route peserta

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::resource('kegiatan', KegiatanController::class);
     // Route::resource('peserta', PesertaController::class);
-    Route::resource('peserta', PesertaController::class);->parameters([
+    Route::resource('peserta', PesertaController::class)->parameters([
     'peserta' => 'peserta'
     ]);
     // Route::resource('anggota', AnggotaController::class);
