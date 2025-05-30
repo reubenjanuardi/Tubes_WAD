6b46b6bb3c48770256c0b2e7787926e33d500604
reubenjanuardi <reubenjanuardi@gmail.com>
2025-05-30 11:05:33 +0700

    create migration table

---
+++ database/migrations/2025_05_30_040142_create_keuangan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('keuangan', function (Blueprint $table) {
+            $table->id();
+            $table->string('kegiatan');
+            $table->enum('jenis', ['pemasukan', 'pengeluaran']);
+            $table->integer('jumlah');
+            $table->text('keterangan')->nullable();
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('keuangan');
+    }
+};
