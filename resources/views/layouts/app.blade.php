017c930ec52159274a9963dad22d0f92fd7c65c6
reubenjanuardi <reubenjanuardi@gmail.com>
2025-06-01 03:04:49 +0700

    Update layout.app
    Add Flash Message for showing error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,10 @@
     <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 m-4 rounded">
         {{ session('success') }}
     </div>
+    @elseif (session('error'))
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 m-4 rounded">
+        {{ session('error') }}
+    </div>
     @endif
 
     <!-- Content -->
