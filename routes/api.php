3373b8021d4298f4e78324db545d6195bb882986
reubenjanuardi <reubenjanuardi@gmail.com>
2025-05-31 16:46:36 +0700

    Update api.php
    add api routes for keuangan

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\KeuanganApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/keuangan', [KeuanganApiController::class, 'index']);
+Route::get('/keuangan/{id}', [KeuanganApiController::class, 'store']);
+Route::get('/keuangan/{id}', [KeuanganApiController::class, 'show']);
+Route::get('/keuangan/{id}', [KeuanganApiController::class, 'update']);
+Route::get('/keuangan/{id}', [KeuanganApiController::class, 'destroy']);
