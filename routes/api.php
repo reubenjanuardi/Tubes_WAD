39f6b9a926b1e19f2ab3c72309eece6113eb0465
Rutuga <m854480@gmail.com>
2025-06-01 15:34:39 +0700

    add API peserta

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PesertaApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('peserta', PesertaApiController::class);
