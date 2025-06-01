f497b4663bf5406a75830964a2712d1406550132
Naufal <naufalrakadilah21@gmail.com>
2025-06-02 01:45:34 +0700

    add route api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\KegiatanApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/kegiatan', [KegiatanApiController::class, 'index']);
+Route::post('/kegiatn', [KegiatanApiController::class, 'store']);
+Route::get('/kegiatan/{id}', [KegiatanApiController::class, 'show']);
+Route::put('/kegiatan/{id}', [KegiatanApiController::class, 'update']);
+Route::delete('/kegiatan/{id}', [KegiatanApiController::class, 'destroy']);
