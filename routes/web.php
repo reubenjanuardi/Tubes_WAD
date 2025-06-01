ce44b42a0ca30f8eea015872b4db7d2bb6eaa1ed
reubenjanuardi <reubenjanuardi@gmail.com>
2025-06-01 22:20:36 +0700

    Update routes.web.php
    Update menyesuakan routing untuk menu yang ada

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KegiatanController;
 use App\Http\Controllers\PesertaController;
+use App\Http\Controllers\InventarisController;
 use App\Http\Controllers\KeuanganController;
-
+use App\Http\Controllers\AnggotaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,14 +29,11 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware('auth');
-    // Route::resource('kegiatan', KegiatanController::class);
-    Route::resource('kegiatan', KegiatanController::class);
-    // Route::resource('peserta', PesertaController::class);
-    Route::resource('peserta', PesertaController::class)->parameters([
-    'peserta' => 'peserta'
-    ]);
-    // Route::resource('anggota', AnggotaController::class);
-    // dst...
-    Route::resource('/keuangan', KeuanganController::class);
+
+    Route::resource('kegiatan', KegiatanController::class)->parameters(['kegiatan' => 'kegiatan']);
+    Route::resource('peserta', PesertaController::class)->parameters(['peserta' => 'peserta']);
+    Route::resource('inventaris', InventarisController::class)->parameters(['inventaris' => 'inventaris']);
+    Route::resource('anggota', AnggotaController::class)->parameters(['anggota' => 'anggota']);
+    Route::resource('keuangan', KeuanganController::class)->parameters(['keuangan' => 'keuangan']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
