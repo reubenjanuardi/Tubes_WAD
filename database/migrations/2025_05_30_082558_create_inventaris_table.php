776aa0b301249771be58a6de69473ffb6d90168c
khadafi <khadafiadisaputra.10@gmail.com>
2025-06-01 22:49:26 +0700

    add create_inventaris_table.php

---
+++ database/migrations/2025_05_30_082558_create_inventaris_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventaris', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->text('deskripsi');
+            $table->date('tanggal');
+            $table->integer('jumlah');
+            $table->string('status')->default('Tersedia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventaris');
+    }
+};
